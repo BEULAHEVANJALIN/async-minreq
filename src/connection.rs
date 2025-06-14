8ee4a89be5febf08dc5d9a239b73cae8b55513a0
psg-19 <pratushgupt22@gmail.com>
2025-06-14 16:46:47 +0530

    fix: TlsStream was never imported in case of rustls feature.

--- src/connection.rs
+++ src/connection.rs
@@ -11,7 +11,7 @@ use {
     rustls_pki_types::ServerName,
     std::convert::TryFrom,
     std::sync::Arc,
-    tokio_rustls::TlsConnector,
+    tokio_rustls::TlsConnector,TlsStream,
 };
 use std::env;
 use std::net::ToSocketAddrs;
@@ -20,7 +20,6 @@ use tokio::io::{self, AsyncRead};
 use tokio::net::TcpStream;
 use tokio::time::{timeout, Instant};
 use std::time::Duration;
-use async_recursion::async_recursion;
 #[cfg(any(feature = "rustls-webpki", feature = "rustls"))]
 use webpki_roots::TLS_SERVER_ROOTS;
 
@@ -238,7 +237,7 @@ impl Connection {
 
     /// Sends the [`Request`](struct.Request.html), consumes this
     /// connection, and returns a [`Response`](struct.Response.html).
-    #[async_recursion]
+
     pub(crate) async fn send(self) -> Result<ResponseLazy, Error> {
         match self.timeout()? {
             None => {
@@ -253,7 +252,7 @@ impl Connection {
         }
     }
 
-    #[async_recursion]
+
     async fn send_without_timeout(mut self) -> Result<ResponseLazy, Error> {
         self.request.url.host = ensure_ascii_host(self.request.url.host)?;
         let bytes = self.request.as_bytes();
@@ -348,7 +347,7 @@ impl Connection {
         Err(Error::AddressNotFound)
     }
 }
-#[async_recursion]
+
 async fn handle_redirects(
     connection: Connection,
     mut response: ResponseLazy,
